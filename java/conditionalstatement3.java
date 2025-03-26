29debf7ad2defdadf37258153a3f6701a35a8bfa
devilord1973 <evilsoul1973@gmail.com>
2025-03-26 18:01:21 +0530

    adding a comment line

--- java/conditionalstatement3.java
+++ java/conditionalstatement3.java
@@ -5,7 +5,7 @@ public class conditionalstatement3 {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-
+        // this is  a if and else condition but it is much similar to else if conditional statement 
         if(a == b){
             System.out.println("equal");
         }else{
