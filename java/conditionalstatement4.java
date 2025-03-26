0cd80432e4ad32a6839080ccbd861b1da70ffea1
devilord1973 <evilsoul1973@gmail.com>
2025-03-26 18:05:19 +0530

    Update conditionalstatement4.java

--- java/conditionalstatement4.java
+++ java/conditionalstatement4.java
@@ -1,1 +1,19 @@
+import java.util.*;
 
+public class conditionalstatement4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        //ELSE IF CONDITIONAL STATEMENT 
+        if(a == b){
+            System.out.println("Equal");
+        }else if(a>b){
+            System.out.println("A is greater ");
+        }else {
+            System.out.println("B is greater ");
+        }
+    }
+    
+}
