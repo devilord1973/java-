969eaa7c2e7581ceb9f2ec1a40e436cf9e0a4599
devilord1973 <evilsoul1973@gmail.com>
2025-03-26 17:57:55 +0530

    Adding a comment line

--- java/conditionalstatement.java
+++ java/conditionalstatement.java
@@ -4,7 +4,7 @@ public class conditionalstatement {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int age = sc.nextInt();
-
+        // IF AND ELSE CONDITIONAL STATEMENT 
         if (age>18) {
             System.out.println("adult");
         } else{
